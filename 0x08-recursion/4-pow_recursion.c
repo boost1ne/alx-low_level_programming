e579956149703a9e4c42d34efce5bd8ed3c10ee7
boost1ne <boustane.chaimaa@gmail.com>
2023-11-16 07:48:18 -0800

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
 * _pow_recursion - number raised to the power of another number
 * @x: the base
 * @y: the exponent
-* Return: given power
+* Return: result of the power
 **/
 
 int _pow_recursion(int x, int y)
@@ -12,6 +12,6 @@ int _pow_recursion(int x, int y)
 	if (y > 0)
 		return (x * _pow_recursion(x, y - 1));
 	if (y < 0)
-		return (1 / _pow_recursion(x, -y));
-	return (1);
+		return (-1);
+	return (-1);
 }
