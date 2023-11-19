7412e1da76fd918e06ba68e67d1d3c386cc0abe3
boost1ne <boustane.chaimaa@gmail.com>
2023-11-19 08:34:18 -0800

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -60,15 +60,15 @@ int main(int argc, char *argv[])
 	int nb1, nb2, result;
 
 
-	if (argc != 3 || argc > 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 
 	}
 
-	nb1 = atoi(argv[1]);
-	nb2 = atoi(argv[2]);
+	nb1 = _atoi(argv[1]);
+	nb2 = _atoi(argv[2]);
 	result = nb1 * nb2;
 
 	printf("%d\n", result);
