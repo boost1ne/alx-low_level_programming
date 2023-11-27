3624c2b82f11fc57e25b59e08859dd469979346f
boost1ne <boustane.chaimaa@gmail.com>
2023-11-27 11:52:32 -0800

    file's name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - that prints the name of the file
+ * followed by a new line
+ * Return: Always 0 (success)
+**/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
