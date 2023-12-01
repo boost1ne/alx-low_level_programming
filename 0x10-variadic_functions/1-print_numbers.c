888bdb2a8ec0c7715db4759bdc69b3afba0e899a
boost1ne <boustane.chaimaa@gmail.com>
2023-12-01 12:28:58 -0800

    numbers and sep

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: a string separates between numbs
+ * @n: number of arguments to be passed and concatenated
+ * @...: Variable arguments to be concatenated and printed
+****/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+
+		printf("%d", va_arg(args, int));
+
+		if (i > 0 && separator != NULL)
+			printf("%s", separator);
+
+	printf("\n");
+
+	va_end(args);
+
+}
