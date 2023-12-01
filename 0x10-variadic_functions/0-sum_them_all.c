a0acb51d15da6a499d08a9d24367ac171234e53e
boost1ne <boustane.chaimaa@gmail.com>
2023-12-01 11:55:13 -0800

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,21 +5,15 @@
  *sum_them_all - variadic func clclts the sum of undef nb of args
  *@n: named given param
  *
- *Return: result of calculated sum
+ *Return: result of calculated sum or 0 of n == 0
 **/
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, sum = 0;
-
 	va_list args;
+	unsigned int i, sum = 0;
 
 	va_start(args, n);
-
-	if (n == 0)
-		va_end(args);
-		return (0);
-
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
