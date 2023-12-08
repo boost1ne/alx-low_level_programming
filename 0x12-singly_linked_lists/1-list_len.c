38385098d4cce1088d676e2caf8ca42bd83271e7
boost1ne <boustane.chaimaa@gmail.com>
2023-12-08 11:39:32 -0800

    test

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,16 +8,14 @@
  *
  * Return: number of elemnts in that list
  * **/
-
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+size_t n = 0;
 
-	while (h)
-	{
-		n++;
-		h = h->next;
-	}
-
-	return (n);
+while (h)
+{
+n++;
+h = h->next;
+}
+return (n);
 }
