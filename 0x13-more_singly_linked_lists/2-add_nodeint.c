183910dfe1e1fdfa9faa1946d4240f76c93efa21
boost1ne <boustane.chaimaa@gmail.com>
2023-12-13 12:45:39 -0800

    new

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,13 +4,13 @@
  * add_nodeint - dds a new node at the beginning of a list
  * @head: pointer to iur lists head
  * @n: element of the list (data part)
- * Return: A new node at the beginning of the list
+ * Return: Adress of a new node at the beginning of the list
  **/
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t newFirst;
+	listint_t *newFirst;
 
-	newFirst = malloc(sizeof(listint_t));
+	*newFirst = malloc(sizeof(listint_t));
 
 	if (!newFirst)
 	{
