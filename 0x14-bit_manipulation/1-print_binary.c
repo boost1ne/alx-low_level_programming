c10e90027ae65221a1cb91f5607003aaa085ef9f
boost1ne <boustane.chaimaa@gmail.com>
2023-12-15 12:15:34 -0800

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -15,13 +15,10 @@ void print_binary(unsigned long int n)
 	{
 		if (n >> 1)
 			print_binary(n >> 1);
-
-		bit = (n & 1) + '0';
-
-		_putchar(res);
+		res = (n & 1) + '0';
+		write(1, &bit, 1);
 	}
 	else
-	{
-		_putchar('0');
+		write(1, "0", 1);
 	}
 }
