59e7b23fe4ef307527725e3dbfcd641ab5ce01ca
boost1ne <boustane.chaimaa@gmail.com>
2023-12-20 15:14:41 -0800

    file desc

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - Read a text file and prints it to the stdout
+ * @filename: Name of the file
+ * @letters: Number of letters to read from the file
+ * Return: The Number of bytes read and write on success, 0 if fails
+ */
+
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t r, w, o;
+
+	char *buff = malloc(sizeof(char) * letters)
+
+	if (!filename)
+		return (0);
+
+
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buff, letters);
+	w = write(STDROUT_FILENO, buff, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buff);
+		return (0);
+	}
+
+	free(buff);
+	close(o);
+
+	return (w);
+
+}
