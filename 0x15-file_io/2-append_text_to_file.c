79eb049d3704c5aa37d3c80bee23cfc95c3f0b30
boost1ne <boustane.chaimaa@gmail.com>
2023-12-21 14:47:10 -0800

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			i++;
 	}
 
-	o = open(filename, O_WONLY | O_APPEND);
+	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, i);
 
 	if (o == -1 || w == -1)
