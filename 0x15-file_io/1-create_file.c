eaa7207fdf7af553ea07f8bc16b80b6c0b65fea6
boost1ne <boustane.chaimaa@gmail.com>
2023-12-21 14:02:59 -0800

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,10 +20,6 @@ int create_file(const char *filename, char *text_content)
 	if (o == -1)
 		return (-1);
 
-	if (text_content == NULL)
-
-		text_content = "";
-
 	if (text_content != NULL)
 	{
 		int i = 0;
