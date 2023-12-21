f65cbce2327e7fc879a1deaab7411415f7cd8657
boost1ne <boustane.chaimaa@gmail.com>
2023-12-21 14:07:47 -0800

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,30 +10,29 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w;
+	int o, w, i = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (o == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		int i = 0;
-
 		while (text_content[i] != '\0')
-	
-	}		i++;
+			i++;
+	}
+	else
+	{	
+		text_content = "";
+	}
 
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, i);
 
-	if (w == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
 	close(o);
 
 	return (1);
+
 }
